fd446e24786ae1ce075f35e8fa0b586bd29e390d
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-04-18 22:33:17 +0530

    ade more functioanalities

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = 9001;
 app.use((req, res, next) => {
   console.log("This is loggermiddlewares made globally");
   const time = new Date();
-  console.log(req.url);
-  console.log(req.path);
+  console.log("url",req.url);
+  console.log("path",req.path);
   console.log("Log at this time :-", time);
   next();
 });
@@ -18,15 +18,20 @@ const checkPermission = (role) => {
       res.send({ route: "/libraries", permission: true });
     } else if (role == "author") {
       res.send({ route: "/authors", permission: true });
+    }else{
+        res.send({err:500});
     }
   };
 };
-
+app.get("/", (req, res) => {
+    res.send("Welcome to Home page my book api");
+  });
 app.get("/books", (req, res) => {
   res.send({ route: "/books" });
 });
 app.get("/libraries", checkPermission("librarian"));
 app.get("/authors", checkPermission("author"));
+app.get("/*", checkPermission(""));
 
 http.createServer(app).listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
